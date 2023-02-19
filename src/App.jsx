4e097187fa6b69a0c0dda5c2a9b36cee3ba30999
lucianobs1 <lucianotetsu@gmail.com>
2023-02-19 15:03:03 -0300

    remove component Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Header } from './components/Header';
-import { Post } from './Post';
 
 import './global.css';
 
@@ -8,10 +7,6 @@ function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Luciano Brandão"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque consectetur porro sint ipsam voluptatibus ad tempora alias. Nihil ipsam, similique eveniet, repudiandae necessitatibus accusamus suscipit non atque neque facere aliquid!"
-      />
     </>
   );
 }
