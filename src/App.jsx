b00738caa372feacfe76fc8fd9193b75a8ec0587
lucianobs1 <lucianotetsu@gmail.com>
2023-02-19 23:12:16 -0300

    add component aside with styles  and component post without styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Header } from './components/Header';
 
+import styles from './App.module.css';
 import './global.css';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Luciano Brandão"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores beatae deserunt veniam. Voluptates enim nam id iste officia maxime possimus quis dicta! Illum, consequatur odio tempora soluta excepturi odit porro."
+          />
+          <Post
+            author="John Doe"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores beatae deserunt veniam. Voluptates enim nam id iste officia maxime possimus quis dicta! Illum, consequatur odio tempora soluta."
+          />
+        </main>
+      </div>
     </>
   );
 }
